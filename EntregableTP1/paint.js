0b27688ef31bd6204f21467dc2fc701a279235a4
Soledad Merino <soledadmerino.1994@gmail.com>
2021-09-13 19:07:16 -0300

    agregados filtro blur y splash color red/green/blue

--- EntregableTP1/paint.js
+++ EntregableTP1/paint.js
@@ -9,6 +9,10 @@ let btnNeg = document.getElementById('btn-neg');
 let btnSepia = document.getElementById('btn-sepia');
 let btnCleanFilter = document.getElementById('btn-clean-filter');
 let btnCleanCanvas = document.getElementById('btn-clean-canvas');
+let btnBlur = document.getElementById('btn-blur');
+let btnSCR = document.getElementById('btn-scr');
+let btnSCG = document.getElementById('btn-scg');
+let btnSCB = document.getElementById('btn-scb');
 
 /* ------------- DRAW ---------------*/
 
@@ -73,7 +77,7 @@ function eraser() {
 
     canvas.addEventListener('mousemove', e => {
         if (isEraser === true) {
-           eraserLine(rangeEraser, ctx, x, y, e.offsetX, e.offsetY);
+            eraserLine(rangeEraser, ctx, x, y, e.offsetX, e.offsetY);
             x = e.offsetX;
             y = e.offsetY;
         }
@@ -122,13 +126,30 @@ btnSepia.addEventListener('click', e => {
     filterSepia();
 })
 
+btnSCR.addEventListener('click', e => {
+    filterSplashColorRed();
+})
+
+btnSCG.addEventListener('click', e => {
+    filterSplashColorGreen();
+})
+
+btnSCB.addEventListener('click', e => {
+    filterSplashColorBlue();
+})
+
+btnBlur.addEventListener('click', e => {
+    filterBlur();
+})
+
 btnCleanFilter.addEventListener('click', e => {
     cleanFilter();
 })
 
-btnCleanCanvas.addEventListener('click', e=> {
+btnCleanCanvas.addEventListener('click', e => {
     cleanCanvas();
 })
+
 
 function showImage() {
     if (cantImg != 0) {
@@ -174,7 +195,7 @@ function filterBaW() {
             let prom = Math.floor((pixel[0] + pixel[1] + pixel[2]) / 3)
             //enviamos el promedio para setear los Bytes r, g y b con el promedio y el alpha en 255
             setPixel(image, x, y, prom, prom, prom, 255);
-            
+
         }
     }
     ctx.putImageData(image, 0, 0) * 4;
@@ -200,11 +221,168 @@ function filterSepia() {
     let image = ctx.getImageData(0, 0, width, height);  //obtenemos la imagen
     for (let x = 0; x <= image.width; x++) {    //la recorremos pixel a pixel
         for (let y = 0; y < image.height; y++) {
-            let pixel = getPixel(image, x, y);  //obtenemos los valores de cada pixel y 
-            let r = (pixel[0] * 0.460 + pixel[1] * 0.470 + pixel[2] * 0.060) ;
-            let g = (pixel[0] * 0.350 + pixel[1] * 0.425 + pixel[2] * 0.070) ;
-            let b = (pixel[0] * 0.270 + pixel[1] * 0.135 + pixel[2] * 0.050) ;
+            let pixel = getPixel(image, x, y);  //obtenemos los valores de cada pixel y lo multiplicamos por los valores necesarios en cada caso
+            let r = (pixel[0] * 0.460 + pixel[1] * 0.470 + pixel[2] * 0.060);
+            let g = (pixel[0] * 0.350 + pixel[1] * 0.425 + pixel[2] * 0.070);
+            let b = (pixel[0] * 0.270 + pixel[1] * 0.135 + pixel[2] * 0.050);
             setPixel(image, x, y, r, g, b, 255);
+        }
+    }
+    ctx.putImageData(image, 0, 0) * 4;
+}
+
+function filterSplashColorRed() {
+    comprobarFiltros();
+    let image = ctx.getImageData(0, 0, width, height);  //obtenemos la imagen
+    for (let x = 0; x <= image.width; x++) {    //la recorremos pixel a pixel
+        for (let y = 0; y < image.height; y++) {
+            let pixel = getPixel(image, x, y);  //obtenemos los valores de cada pixel y calculamos el promedio
+            if (pixel[0] > pixel[1] && pixel[0] > pixel[2]) {
+                setPixel(image, x, y, pixel[0], pixel[1], pixel[2], 255);
+            } else {
+            let prom = Math.floor((pixel[0] + pixel[1] + pixel[2]) / 3)
+            //enviamos el promedio para setear los Bytes r, g y b con el promedio y el alpha en 255
+            setPixel(image, x, y, prom, prom, prom, 255);
+            }
+        }
+    }
+    ctx.putImageData(image, 0, 0) * 4;
+}
+
+function filterSplashColorGreen() {
+    comprobarFiltros();
+    let image = ctx.getImageData(0, 0, width, height);  //obtenemos la imagen
+    for (let x = 0; x <= image.width; x++) {    //la recorremos pixel a pixel
+        for (let y = 0; y < image.height; y++) {
+            let pixel = getPixel(image, x, y);  //obtenemos los valores de cada pixel y calculamos el promedio
+            if (pixel[1] > pixel[0] && pixel[1] > pixel[2]) {
+                setPixel(image, x, y, pixel[0], pixel[1], pixel[2], 255);
+            } else {
+            let prom = Math.floor((pixel[0] + pixel[1] + pixel[2]) / 3)
+            //enviamos el promedio para setear los Bytes r, g y b con el promedio y el alpha en 255
+            setPixel(image, x, y, prom, prom, prom, 255);
+            }
+        }
+    }
+    ctx.putImageData(image, 0, 0) * 4;
+}
+
+function filterSplashColorBlue() {
+    comprobarFiltros();
+    let image = ctx.getImageData(0, 0, width, height);  //obtenemos la imagen
+    for (let x = 0; x <= image.width; x++) {    //la recorremos pixel a pixel
+        for (let y = 0; y < image.height; y++) {
+            let pixel = getPixel(image, x, y);  //obtenemos los valores de cada pixel y calculamos el promedio
+            if (pixel[2] > pixel[0] && pixel[2] > pixel[1]) {
+                setPixel(image, x, y, pixel[0], pixel[1], pixel[2], 255);
+            } else {
+            let prom = Math.floor((pixel[0] + pixel[1] + pixel[2]) / 3)
+            //enviamos el promedio para setear los Bytes r, g y b con el promedio y el alpha en 255
+            setPixel(image, x, y, prom, prom, prom, 255);
+            }
+        }
+    }
+    ctx.putImageData(image, 0, 0) * 4;
+}
+
+function filterBlur() {
+    comprobarFiltros();
+    let image = ctx.getImageData(0, 0, width, height);  //obtenemos la imagen
+    let r = 0;
+    let g = 0;
+    let b = 0;
+    for (let x = 0; x <= image.width; x++) {    //la recorremos pixel a pixel contemplando las posibilidades
+        if (x === 0) {      //limitamos la cantidad de comprobaciones que se debe hacer en cada caso
+            for (let y = 0; y < image.height; y++) {
+                if (y === 0) {    //en cada caso particular, obtenemos el promedio de los valores vecinos a la posición actual
+                    let xy1 = getPixel(originalImage, x, y+1);
+                    let x1y = getPixel(originalImage, x+1, y);
+                    let x1y1 = getPixel(originalImage, x+1, y+1);
+                    r = (xy1[0] + x1y[0] + x1y1[0]) /3;
+                    g = (xy1[1] + x1y[1] + x1y1[1]) /3;
+                    b = (xy1[2] + x1y[2] + x1y1[2]) /3;
+                } else if (y === image.height - 1) {
+                    let xy0 = getPixel(originalImage, x, y-1);
+                    let x1y = getPixel(originalImage, x+1, y);
+                    let x1y0 = getPixel(originalImage, x+1, y-1);
+                    r = (xy0[0] + x1y[0] + x1y0[0]) /3;
+                    g = (xy0[1] + x1y[1] + x1y0[1]) /3;
+                    b = (xy0[2] + x1y[2] + x1y0[2]) /3;
+                } else {
+                    let xy0 = getPixel(originalImage, x, y-1);
+                    let x1y0 = getPixel(originalImage, x+1, y-1);
+                    let x1y = getPixel(originalImage, x+1, y);
+                    let x1y1 = getPixel(originalImage, x+1, y+1);
+                    let xy1 = getPixel(originalImage, x, y+1);
+                    r = (xy0[0] + x1y0[0] + x1y[0] + x1y1[0] + xy1[0]) /5;
+                    g = (xy0[1] + x1y0[1] + x1y[1] + x1y1[1] + xy1[1]) /5;
+                    b = (xy0[2] + x1y0[2] + x1y[2] + x1y1[2] + xy1[2]) /5;
+                }
+                setPixel(image, x, y, r, g, b, 255);
+            }
+        } else if (x === image.width - 1) {
+            for (let y = 0; y < image.height; y++) {
+                if (y === 0) {
+                    let x0y = getPixel(originalImage, x-1, y);
+                    let x0y1 = getPixel(originalImage, x-1, y+1);
+                    let xy1 = getPixel(originalImage, x, y+1);
+                    r = (x0y[0] + x0y1[0] + xy1[0]) /3;
+                    g = (x0y[1] + x0y1[1] + xy1[1]) /3;
+                    b = (x0y[2] + x0y1[2] + xy1[2]) /3;
+                } else if (y === image.height - 1) {
+                    let x0y = getPixel(originalImage, x-1, y);
+                    let x0y0 = getPixel(originalImage, x-1, y-1);
+                    let xy0 = getPixel(originalImage, x, y-1);
+                    r = (x0y[0] + x0y0[0] + xy0[0]) /3;
+                    g = (x0y[1] + x0y0[1] + xy0[1]) /3;
+                    b = (x0y[2] + x0y0[2] + xy0[2]) /3;
+                } else {
+                    let xy0 = getPixel(originalImage, x, y-1);
+                    let x0y0 = getPixel(originalImage, x-1, y-1);
+                    let x0y = getPixel(originalImage, x-1, y);
+                    let x0y1 = getPixel(originalImage, x-1, y+1);
+                    let xy1 = getPixel(originalImage, x, y+1);
+                    r = (xy0[0] + x0y0[0] + x0y[0] + x0y1[0] + xy1[0]) /5;
+                    g = (xy0[1] + x0y0[1] + x0y[1] + x0y1[1] + xy1[1]) /5;
+                    b = (xy0[2] + x0y0[2] + x0y[2] + x0y1[2] + xy1[2]) /5;
+                }
+                setPixel(image, x, y, r, g, b, 255);
+            }
+        } else {
+            for (let y = 0; y < image.height; y++) {
+                if (y === 0) {
+                    let xy0 = getPixel(originalImage, x, y-1);
+                    let x0y1 = getPixel(originalImage, x-1, y+1);
+                    let xy1 = getPixel(originalImage, x, y+1);
+                    let x1y1 = getPixel(originalImage, x+1, y+1);
+                    let x1y = getPixel(originalImage, x+1, y);
+                    r = (xy0[0] + x1y1[0] + x1y[0] + x0y1[0] + xy1[0]) /5;
+                    g = (xy0[1] + x1y1[1] + x1y[1] + x0y1[1] + xy1[1]) /5;
+                    b = (xy0[2] + x1y1[2] + x1y[3] + x0y1[2] + xy1[2]) /5;
+                } else if (y === image.height-1) {
+                    let x0y = getPixel(originalImage, x-1, y);
+                    let x0y0 = getPixel(originalImage, x-1, y-1);
+                    let xy0 = getPixel(originalImage, x, y-1);
+                    let x1y0 = getPixel(originalImage, x+1, y-1);
+                    let x1y = getPixel(originalImage, x+1, y);
+                    r = (xy0[0] + x0y[0] + x1y[0] + x0y0[0] + x1y0[0]) /5;
+                    g = (xy0[1] + x0y[1] + x1y[1] + x0y0[1] + x1y0[1]) /5;
+                    b = (xy0[2] + x0y[2] + x1y[2] + x0y0[2] + x1y0[2]) /5;
+                } else {
+                    let x0y0 = getPixel(originalImage, x-1, y-1);
+                    let xy0 = getPixel(originalImage, x, y-1);
+                    let x1y0 = getPixel(originalImage, x+1, y-1);
+                    let x1y = getPixel(originalImage, x+1, y);
+                    let x1y1 = getPixel(originalImage, x+1, y+1);
+                    let xy1 = getPixel(originalImage, x, y+1);
+                    let x0y1 = getPixel(originalImage, x-1, y+1);
+                    let x0y = getPixel(originalImage, x-1, y);
+                    r = (xy0[0] + x0y[0] + x1y[0] + x0y0[0] + x1y0[0] + x1y1[0] + xy1[0] + x0y1[0]) /8;
+                    g = (xy0[1] + x0y[1] + x1y[1] + x0y0[1] + x1y0[1] + x1y1[1] + xy1[1] + x0y1[1]) /8;
+                    b = (xy0[2] + x0y[2] + x1y[2] + x0y0[2] + x1y0[2] + x1y1[2] + xy1[2] + x0y1[2]) /8;
+                }
+                setPixel(image, x, y, r, g, b, 255);
+            }
         }
     }
     ctx.putImageData(image, 0, 0) * 4;
@@ -251,7 +429,7 @@ function saveOriginalImage() {
 function cleanFilter() {
     if (aplicatedFilters) {
         let image = ctx.getImageData(0, 0, width, height);  //obtenemos la imagen
-        image = originalImage;
+        image = originalImage;  //la convertimos en la imagen original
         ctx.putImageData(image, 0, 0) * 4;
         aplicatedFilters = false;
         originalImage = ctx.createImageData(width, height);
@@ -260,7 +438,7 @@ function cleanFilter() {
 
 //se borra todo el lienzo y se deja como al inicio
 function cleanCanvas() {
-    canvas.width = width;
+    canvas.width = width;   //seteamos el ancho y el alto originales de nuestro canvas inicial
     canvas.height = height;
     let image = ctx.getImageData(0, 0, width, height);  //obtenemos la imagen
     for (let x = 0; x <= image.width; x++) {    //la recorremos pixel a pixel
