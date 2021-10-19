9e4f05964138752047ad9eea9ec4deac4e5d290c
Soledad Merino <soledadmerino.1994@gmail.com>
2021-10-19 19:25:41 -0300

    Agregado sprite sheet de muerte. Agrandado un poco el páralax. Arreglado deslizamiento

--- EntregableTP3/js/main.js
+++ EntregableTP3/js/main.js
@@ -3,13 +3,13 @@
 window.onkeyup = function (event){
     let avatar = document.getElementById('avatar');
     let interval;
-    if (event.keyCode === 32) { // 32= barra espaciadora, 38 = up-arrow
+    if (event.keyCode === 32 || event.keyCode === 38 || event.keyCode === 87) { // 32= barra espaciadora, 38 = up-arrow
         avatar.classList.replace('avatar', 'saltando');
         interval = setInterval(() => {
             avatar.classList.replace('saltando', 'avatar');
             clearInterval(interval);
         }, 800);
-    }else if (event.keyCode === 38) { // 32= barra espaciadora, 38 = up-arrow
+    }else if (event.keyCode === 40 || event.keyCode === 83) { // 40 = down-arrow
         avatar.classList.replace('avatar', 'deslizando'); //arreglar deslizando
         interval = setInterval(() => {
             avatar.classList.replace('deslizando', 'avatar');
