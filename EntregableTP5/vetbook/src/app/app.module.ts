69c2555dafeea020c6f42186e93e3585b0a8685d
Soledad Merino <soledadmerino.1994@gmail.com>
2021-11-24 18:26:15 -0300

    Empezado código red social

---
+++ EntregableTP5/vetbook/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MaterialModule } from './modules/material/material.module';
+import { PostComponent } from './posts/post/post.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+    PostComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
