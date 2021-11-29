2b39f57c9c4edec2aec9071ae90f8d5bba2d4c07
Soledad Merino <soledadmerino.1994@gmail.com>
2021-11-29 13:34:34 -0300

    Agregado funcionalidad de dar like y quitar like

--- EntregableTP5/vetbook/src/app/modules/main-layout/posts/reaction/reaction.component.ts
+++ EntregableTP5/vetbook/src/app/modules/main-layout/posts/reaction/reaction.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { PostService } from 'src/app/services/post.service';
 
 @Component({
     selector: 'app-reaction',
@@ -9,9 +10,10 @@ export class ReactionComponent implements OnInit {
 
     @Input() type: String = "";
     @Input() quantity: number = 0;
+    @Input() postId: number = 0;
     pathImg: String = "";
 
-    constructor() { }
+    constructor(private postSvc: PostService) { }
 
     ngOnInit(): void {
         if (this.type == "like") {
@@ -20,6 +22,22 @@ export class ReactionComponent implements OnInit {
             this.pathImg = "../../../../../assets/posts/comentar.svg";
         } else if (this.type == "share") {
             this.pathImg = "../../../../../assets/posts/compartir.svg";
+        }
+    }
+
+    react() {
+        if (this.type == "like") {
+            if (this.postSvc.isLiked(this.postId)) {
+                this.postSvc.likeDown(this.postId);
+                this.pathImg = "../../../../../assets/posts/megusta.svg";
+            } else {
+                this.postSvc.likeUp(this.postId);
+                this.pathImg = "../../../../../assets/posts/megustalleno.svg";
+            }
+        } else if (this.type == "comment") {
+            
+        } else if (this.type == "share") {
+            
         }
     }
 
--- EntregableTP5/vetbook/src/app/services/post.service.ts
+++ EntregableTP5/vetbook/src/app/services/post.service.ts
@@ -12,24 +12,69 @@ const POSTS: Post[] = [
         video: "",
         usersIdLiked: [],
         commentsList: [],
-        usersIdShared: [], 
+        usersIdShared: [],
     } //agregar más posts
 ]
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class PostService {
 
-  constructor() { }
+    constructor() { }
 
-  //Devuelve todos los posts, se usa en el home
-  getPosts(): Post[] {
-    return POSTS;
-  }
+    //Devuelve todos los posts, se usa en el home
+    getPosts(): Post[] {
+        return POSTS;
+    }
 
-  //Devuelve los post de un solo usuario, se usa en el perfil
-  getPostsByUserId(userId: number): Post[] {
-      return POSTS.filter(p => p.userId === userId);
-  }
+    //Devuelve los post de un solo usuario, se usa en el perfil
+    getPostsByUserId(userId: number): Post[] {
+        return POSTS.filter(p => p.userId === userId);
+    }
+
+    getPost(postId: number): Post {
+        let post = POSTS.find(p => p.id === postId);
+        if (post != null) {
+            return post;
+        }
+        return {
+            id: 0,
+            date: new Date(),
+            userId: 0,
+            userName: "",
+            text: "",
+            images: [],
+            video: "",
+            usersIdLiked: [],
+            commentsList: [],
+            usersIdShared: [],
+        };
+    }
+
+    likeUp(postId: number) {
+        let post: Post = this.getPost(postId);
+        let userId = localStorage.getItem('userId');
+        if (userId != null) {
+            post.usersIdLiked.push(parseInt(userId));
+        }
+    }
+
+    likeDown(postId: number) {
+        let post: Post = this.getPost(postId);
+        let userId = localStorage.getItem('userId');
+        if (userId != null) {
+            let i = post.usersIdLiked.indexOf(parseInt(userId));
+            post.usersIdLiked.splice(i, 1);
+        }
+    }
+
+    isLiked(postId: number): boolean {
+        let post: Post = this.getPost(postId);
+        let userId = localStorage.getItem('userId');
+        if (userId != null) {
+            return post.usersIdLiked.includes(parseInt(userId))
+        }
+        return false;
+    }
 }
